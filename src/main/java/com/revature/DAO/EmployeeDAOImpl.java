917f0b45fe2b977dee46d854ea1c5c826d9b6c8d
HanPark-Ethan <ethan.hanp89@gmail.com>
2022-01-25 15:03:13 -0500

    Created EmployeeDAOImlp

---
+++ src/main/java/com/revature/DAO/EmployeeDAOImpl.java
@@ -0,0 +1,17 @@
+package com.revature.DAO;
+
+import com.revature.models.Reimbursement;
+
+import java.util.List;
+
+public class EmployeeDAOImpl implements EmployeeDAO{
+    @Override
+    public List<Reimbursement> viewPastRequest() {
+        return null;
+    }
+
+    @Override
+    public Reimbursement reimbRequest(Reimbursement r) {
+        return null;
+    }
+}
