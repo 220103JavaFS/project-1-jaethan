91e6230d9742f34dbf8e08d806e4382ee9e94df3
HanPark-Ethan <ethan.hanp89@gmail.com>
2022-01-27 16:26:52 -0500

    Working on EmployeeDAOImpl.. will make changes again later tonight

--- src/main/java/com/revature/DAO/EmployeeDAOImpl.java
+++ src/main/java/com/revature/DAO/EmployeeDAOImpl.java
@@ -1,7 +1,11 @@
 package com.revature.DAO;
 
 import com.revature.models.Reimbursement;
+import com.revature.utils.ConnectionUtil;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class EmployeeDAOImpl implements EmployeeDAO{
@@ -12,6 +16,23 @@ public class EmployeeDAOImpl implements EmployeeDAO{
 
     @Override
     public Reimbursement reimbRequest(Reimbursement r) {
-        return null;
+        try (Connection conn = ConnectionUtil.getConnection()) {
+            String sql = "INSERT INTO ers_reimbursement (reimb_amount, reimb_submitted, reimb_description, reimb_author, reimb_status_id, reimb_type_id)" +
+                    "VALUES (? AS INTEGER, (SELECT CURRENT_TIMESTAMP), ?, ?, 1, );";
+
+            PreparedStatement statement = conn.prepareStatement(sql);
+
+            int count = 0;
+            statement.setString(++count, Integer.toString(r.getReimbAmount()));
+            statement.setString(++count, Long.toString(r.getReimbSubmitted()));
+            statement.setString(++count, r.getReimbDescription());
+            statement.execute();
+
+            return r;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return r;
     }
 }
