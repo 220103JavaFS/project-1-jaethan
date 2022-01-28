d10591fa24dae37d43b7e7e00d7ad4e1898cee82
hm9811 <hm981198@gmail.com>
2022-01-28 12:39:07 -0800

    refactored manager files

---
+++ src/main/java/com/revature/controllers/EmployeeController.java
@@ -0,0 +1,10 @@
+package com.revature.controllers;
+
+import io.javalin.Javalin;
+
+public class EmployeeController implements Controller{
+    @Override
+    public void addRoutes(Javalin app) {
+
+    }
+}
