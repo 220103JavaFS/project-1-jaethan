c61c47de897dd8705e88e58bececcae06a8db3dc
hm9811 <hm981198@gmail.com>
2022-01-29 10:18:52 -0800

    we actually needed finbyId for employee and manager DAO so added it back up.

--- src/main/java/com/revature/DAO/UserDAO.java
+++ src/main/java/com/revature/DAO/UserDAO.java
@@ -4,4 +4,5 @@ import com.revature.models.Users;
 
 public interface UserDAO {
     Users findbyName(String userName);
+    Users findbyId(int userId);
 }
--- src/main/java/com/revature/DAO/UserDAOImpl.java
+++ src/main/java/com/revature/DAO/UserDAOImpl.java
@@ -36,4 +36,33 @@ public class UserDAOImpl implements UserDAO{
         }
         return new Users();
     }
+
+    @Override
+    public Users findbyId(int userId) {
+        try(Connection conn = ConnectionUtil.getConnection()){
+            String sql = "SELECT * FROM (SELECT * FROM ers_users LEFT JOIN ers_user_roles ON ers_users_roles.ers_user_role_id = ers_users.user_role_id)" +
+                    " AS role_id WHERE ers_users_id = " + userId + ";";
+
+            Statement statement = conn.createStatement();
+            ResultSet result = statement.executeQuery(sql);
+
+            Users user = new Users();
+
+            while(result.next()){
+                user.setUserId(result.getInt("ers_users_id"));
+                user.setUsername(result.getString("ers_username"));
+                user.setPassword(result.getString("ers_password"));
+                user.setUserFirstName(result.getString("user_first_name"));
+                user.setUserLastName(result.getString("user_last_name"));
+                user.setEmail(result.getString("user_email"));
+                Roles role = new Roles(result.getInt("ers_user_role_id"),
+                        result.getString("user_role"));
+                user.setRoleId(role);
+            }
+            return user;
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return new Users();
+    }
 }
