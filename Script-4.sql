bb2badb2514062db5cb148b9724a0e50f1aefae1
Jaehyun Lewis <72472484+hm9811@users.noreply.github.com>
2022-01-29 14:29:02 -0800

    changed script

--- Script-4.sql
+++ Script-4.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS	ers_reimbursement;
+DROP TABLE IF EXISTS	ers_users;
 DROP TABLE IF EXISTS	ers_reimbursement_status;
 DROP TABLE IF EXISTS	ers_reimbursement_type;
-DROP TABLE IF EXISTS	ers_users;
 DROP TABLE IF EXISTS	ers_user_roles;
 
 CREATE TABLE ers_reimbursement_status(
@@ -14,19 +14,6 @@ CREATE TABLE ers_reimbursement_type(
 	reimb_type	VARCHAR(10)
 );
 
-CREATE TABLE ers_reimbursement(
-	reimb_id SERIAL 	PRIMARY KEY,
-	reimb_amount		INT,
-	reimb_submitted		TIMESTAMP,
-	reimb_resolved		TIMESTAMP,
-	reimb_description	VARCHAR(250),
-	reimb_receipt		BYTEA,
-	reimb_author		INT REFERENCES ers_user_users(user_role_id),
-	reimb_resolver		INT	REFERENCES ers_user_users(user_role_id),
-	reimb_status_id		INT REFERENCES ers_reimbursement_status(reimb_status_id),
-	reimb_type_id		INT REFERENCES ers_reimbursement_type(reimb_type_id)
-);
-
 CREATE TABLE ers_user_roles (
 	ers_user_role_id SERIAL PRIMARY KEY,
 	user_role VARCHAR (50)
@@ -34,10 +21,23 @@ CREATE TABLE ers_user_roles (
 
 CREATE TABLE ers_users(
 	ers_users_id SERIAL PRIMARY KEY,
-	ers_username		UNIQUE	VARCHAR(50),
-	ers_password		VARCHAR(50),
+	ers_username		VARCHAR(50)		UNIQUE,
+	ers_password		VARCHAR(200),
 	user_first_name		VARCHAR(100),
 	user_last_name		VARCHAR(100),
-	user_email			UNIQUE	VARCHAR(150),
+	user_email			VARCHAR(150) 	UNIQUE,
 	user_role_id		INT	REFERENCES ers_user_roles(ers_user_role_id)
+);
+
+CREATE TABLE ers_reimbursement(
+	reimb_id SERIAL 	PRIMARY KEY,
+	reimb_amount		INT,
+	reimb_submitted		TIMESTAMP,
+	reimb_resolved		TIMESTAMP,
+	reimb_description	VARCHAR(250),
+	reimb_receipt		BYTEA,
+	reimb_author		INT REFERENCES ers_users(ers_users_id),
+	reimb_resolver		INT	REFERENCES ers_users(ers_users_id),
+	reimb_status_id		INT REFERENCES ers_reimbursement_status(reimb_status_id),
+	reimb_type_id		INT REFERENCES ers_reimbursement_type(reimb_type_id)
 );
