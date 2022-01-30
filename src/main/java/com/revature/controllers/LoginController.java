8f074aed8851e66d06497cbbc352b93c4ed49304
HanPark-Ethan <ethan.hanp89@gmail.com>
2022-01-30 16:42:05 -0500

    added Logger for LoginController

--- src/main/java/com/revature/controllers/LoginController.java
+++ src/main/java/com/revature/controllers/LoginController.java
@@ -4,17 +4,22 @@ import com.revature.models.LoginDTO;
 import com.revature.services.LoginService;
 import io.javalin.Javalin;
 import io.javalin.http.Handler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LoginController implements Controller{
     private LoginService loginService = new LoginService();
+    private static final Logger loginLog = LoggerFactory.getLogger(LoginService.class);
 
     private Handler login = (ctx) -> {
         LoginDTO user = ctx.bodyAsClass(LoginDTO.class); // A DTO (Data transfer object) is a tempory object used just to communicate information.
 
         if(loginService.login(user.username, user.password)){
+            loginLog.info("Login successful");
             ctx.req.getSession();
             ctx.status(200);
         }else {
+            loginLog.info("Login failed");
             ctx.req.getSession().invalidate();
             ctx.status(401);
         }
