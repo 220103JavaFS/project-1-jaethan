e274dd441fbe4e542d3b3e07f6153a8e524048e4
HanPark-Ethan <ethan.hanp89@gmail.com>
2022-02-02 10:31:51 -0500

    added some async functions

--- frontEnd/JS/employeeTemp.js
+++ frontEnd/JS/employeeTemp.js
@@ -1,19 +1,39 @@
 let requestBtn = document.getElementById("requestBtn");
 let searchBtn = document.getElementById("searchBtn");
+let viewRequests = document.getElementById("viewPastRequest");
 
 const url = "http://localhost:7000/"
 
-async function getPastReimb(){
-let userId = 
+requestBtn.addEventListener("click", requestFunc);
+searchBtn.addEventListener("click", searchFunc);
+viewRequests.addEventListener("click", getPastReimb);
 
-    let response = await fetch(url + "employee/", {
+async function getPastReimb(){
+let userId = something.value;
+
+    let response = await fetch(
+        url + "employee/" + userId, 
+        {
         credentials: "include"
     });
 
     if (response.status === 200) {
-        let homes = await response.json();
-        populateHomes(homes);
+        let reimb = await response.json();
+        listReimb(reimb);
     }else{
-        console.log("There was an error getting your homes")
+        console.log("error getting the past reimbursement records")
+    }
+}
+
+function listReimb(reimbursement){
+    viewRequests.innerText = "";
+    for (let reimb of reimbursement){
+        let row = document.createElement("tr");
+        for (let data in reimb){
+            let td = document.createElement("td");
+            td.innerText = reimb[data];
+            row.appendChild(td);
+        }
+        viewRequests.appendChild(row);
     }
 }
